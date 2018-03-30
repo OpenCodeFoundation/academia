15a72b90367ade1b91589e8d298df41a4b6db86e
Ratan Sunder Parai <ratanparai@gmail.com>
2018-03-30 16:54:18 +0600

    create generic repository

---
+++ src/Academia.Core/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,19 @@
+﻿using Academia.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academia.Core.Interfaces
+{
+    public interface IAsyncRepository<T> where T : Entity
+    {
+        Task<T> GetByIdAsync(Guid id);
+
+        Task<T> AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ src/Academia.Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,70 @@
+﻿using Academia.Core.Interfaces;
+using Academia.Core.SharedKernel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academia.Infrastructure.Data
+{
+    public class EfRepository<T> : IRepository<T>, IAsyncRepository<T> where T : Entity
+    {
+        protected AcademiaContext _dbContext;
+
+        public EfRepository(AcademiaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public T Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+
+            return entity;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public T GetById(Guid id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
