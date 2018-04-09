05b32d96bf986ab091cbd0c804752f89dc427b66
Ratan Sunder Parai <ratanparai@gmail.com>
2018-04-09 21:22:46 +0600

    remove autogenerated contents

--- src/Academia.Web/ClientApp/src/app/app.module.ts
+++ src/Academia.Web/ClientApp/src/app/app.module.ts
@@ -6,17 +6,11 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    NavMenuComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
