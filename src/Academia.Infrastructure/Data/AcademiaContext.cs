49c99b2e9703c410d511fce22ad5d147eb545a98
Ratan Sunder Parai <ratanparai@gmail.com>
2018-04-29 20:29:12 +0600

    add migration

--- src/Academia.Infrastructure/Data/AcademiaContext.cs
+++ src/Academia.Infrastructure/Data/AcademiaContext.cs
@@ -1,5 +1,6 @@
 ﻿using Academia.Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,5 +15,17 @@ namespace Academia.Infrastructure.Data
         }
 
         public DbSet<Institute> Institutes { get; set; }
+    }
+
+    // https://docs.microsoft.com/en-us/ef/core/miscellaneous/cli/dbcontext-creation
+    public class AcademiaContextDesignFactory : IDesignTimeDbContextFactory<AcademiaContext>
+    {
+        public AcademiaContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AcademiaContext>()
+                .UseSqlServer("Server=.;Initial Catalog=Techcombd.AcademiaDb;Integrated Security=true");
+
+            return new AcademiaContext(optionsBuilder.Options);
+        }
     }
 }
---
+++ src/Academia.Web/IWebHostExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Academia.Web
+{
+    public static class IWebHostExtensions
+    {
+        public static IWebHost MigrateDbContext<TContext>(this IWebHost webHost, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation($"Migrating database associated with context { typeof(TContext).Name}");
+
+                    var retry = Policy.Handle<SqlException>()
+                        .WaitAndRetry(new TimeSpan[]
+                        {
+                            TimeSpan.FromSeconds(5),
+                            TimeSpan.FromSeconds(10),
+                            TimeSpan.FromSeconds(15)
+                        });
+
+                    retry.Execute(() =>
+                    {
+                        context.Database.Migrate();
+                        seeder(context, services);
+                    });
+
+                    logger.LogInformation($"Migrated database associated with context { typeof(TContext).Name}");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, $"An error occurred while migrating the databases used on context { typeof(TContext).Name}");
+                }
+            }
+
+            return webHost;
+        }
+    }
+}
