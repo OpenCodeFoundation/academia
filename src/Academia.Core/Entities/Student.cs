14b99758cc4332803e5bb6f75e4cbfdcec5963ca
MD. Shohag Mia <mdshohagmia14@outlook.com>
2018-05-20 21:46:34 +0600

    Added Student Entity

---
+++ src/Academia.Core/Entities/Student.cs
@@ -0,0 +1,12 @@
+﻿using Academia.Core.SharedKernel;
+using System;
+
+namespace Academia.Core.Entities
+{
+    public class Student : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
+    }
+}
