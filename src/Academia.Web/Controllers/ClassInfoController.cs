2ded0adeb8a21ff54833bd41d7f42d3695832d26
hrakibul <hrakibul@aexcellences.com>
2018-07-11 10:47:01 +0600

    Class and Controller added for ClassInfo

---
+++ src/Academia.Web/Controllers/ClassInfoController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Academia.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ClassInfo")]
+    public class ClassInfoController : Controller
+    {
+    }
+}
