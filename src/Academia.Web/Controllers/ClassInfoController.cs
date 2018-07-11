03f3bba6c5c21e5ddb8221a8135a34b297ec34bd
hrakibul <hrakibul@aexcellences.com>
2018-07-11 12:32:37 +0600

    GetAll method added in ClassInfo Controller

--- src/Academia.Web/Controllers/ClassInfoController.cs
+++ src/Academia.Web/Controllers/ClassInfoController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Academia.Core.Entities;
+using Academia.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Academia.Web.Controllers
 {
@@ -6,5 +10,17 @@ namespace Academia.Web.Controllers
     [Route("api/ClassInfo")]
     public class ClassInfoController : Controller
     {
+        private readonly IAsyncRepository<ClassInfo> _classInfoRepository;
+
+        public ClassInfoController(IAsyncRepository<ClassInfo> classInfoRepository)
+        {
+            _classInfoRepository = classInfoRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ClassInfo>> GetAll()
+        {
+            return await _classInfoRepository.ListAllAsync();
+        }
     }
 }
